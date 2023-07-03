4a97b70a90eee1f4384d469581fd0c014e982766
N2xPun <134921719+N2xPun@users.noreply.github.com>
2023-07-03 15:50:45 +0700

    Update cs01grading.py

--- cs01grading.py
+++ cs01grading.py
@@ -3,22 +3,25 @@ mScore = int(input("Please enter your midterm score: "))
 fScore = int(input("Please enter your final score: "))
 
 score = wScore + mScore + fScore
+grade = "invalid"
 
 if (score >= 80 and score <= 100):
-    print("Your grade is A.")
+    grade = "A"
 elif (score <= 79 and score >= 75):
-    print("Your grade is B+.")
+    grade = "B+"
 elif (score <= 74 and score >= 70):
-    print("Your grade is B.")
+    grade = "B"
 elif (score <= 69 and score >= 65):
-    print("Your grade is C+.")
+    grade = "C+"
 elif (score <= 64 and score >= 60):
-    print("Your grade is C.")
+    grade = "C"
 elif (score <= 59 and score >= 55):
-    print("Your grade is D+.")
+    grade = "D+"
 elif (score <= 54 and score >= 50):
-    print("Your grade is D.")
+    grade = "D"
 elif (score <= 49 and score >= 0):
-    print("Your grade is F.")
+    grade = "F"
 else:
     print("Your score is invalid.")
+
+print("Your grade is ", grade, ".")
