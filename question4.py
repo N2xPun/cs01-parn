13c8086ec5e57d8bb788c783fed635b5a0bd75b4
N2xPun <134921719+N2xPun@users.noreply.github.com>
2023-07-03 16:34:20 +0700

    fixq4

--- question4.py
+++ question4.py
@@ -1,6 +1,6 @@
-x = float(input("Please enter a number: "))
+x = int(input("Please enter a number: "))
 
-if (x<10 and x.is_integer):
+if (x<10 and x>=0):
     print("The number has a single digit.")
 else:
     print("The number has more than one digit.")
