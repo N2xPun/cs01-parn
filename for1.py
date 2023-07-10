e7441ab4c6a21c8a9614da694a43c9d64bfe22f1
N2xPun <134921719+N2xPun@users.noreply.github.com>
2023-07-10 16:26:26 +0700

    allforquest

---
+++ for1.py
@@ -0,0 +1,2 @@
+n = int(input("Please input a number: "))
+print("Result:", int(n*(n+1)/2))
---
+++ for3.py
@@ -0,0 +1,6 @@
+n = int(input("Please input a number: "))
+
+r = ""
+for i in range(1,n+1,2):
+    r += str(i) + " "
+print(r)
