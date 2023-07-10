7eca31dacca80ef0135999a525f26586e9ea72f1
N2xPun <134921719+N2xPun@users.noreply.github.com>
2023-07-10 15:20:19 +0700

    11LinesButCooler

--- cs01grading.py
+++ cs01grading.py
@@ -1,11 +1,11 @@
-wScore = int(input("Please enter your work score: "))
-mScore = int(input("Please enter your midterm score: "))
-fScore = int(input("Please enter your final score: "))
-grades = ["D","D+","C","C+","B","B+"]
-score = int((wScore + mScore + fScore-50)/5)
-if (score < 0):
-    print("Your grade is F.")
-elif (score >= 80):
-    print("Your grade is A.")
-else:
-    print("Your grade is " + str(grades[score]) + ".")
+def sInput(min,max,text):
+    outScore = -1
+    while(outScore < min or outScore > max):
+        outScore = int(input("Please enter your " + str(text) + " score: "))
+    return outScore
+wScore = sInput(0,30,"work")
+mScore = sInput(0,30,"midterm")
+fScore = sInput(0,40,"final")
+grades = ["F","F","F","F","F","F","F","F","F","F","D","D+","C","C+","B","B+","A","A","A","A"]
+score = int((wScore + mScore + fScore)/5)
+print("Your grade is " + str(grades[score]) + ".")
